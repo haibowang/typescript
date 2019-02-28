39222a384b9b7f65d8d58b07706c614b1772161f
hwang <haibo.wang@pfandbriefbank.com>
2019-02-28 16:43:11 +0100

    add product-detail, product-item

--- ngAuction/src/app/product-item/product-item.component.ts
+++ ngAuction/src/app/product-item/product-item.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../shared/product.service';
 
 @Component({
-  selector: 'nga-product-item',
-  templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+    selector: 'nga-product-item',
+    templateUrl: './product-item.component.html',
+    styleUrls: ['./product-item.component.css']
 })
 export class ProductItemComponent implements OnInit {
 
-  constructor() { }
+    @Input() product: Product;
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+    }
 }
